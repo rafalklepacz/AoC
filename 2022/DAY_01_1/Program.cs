f4e1ba9e741679aff4e25dc98f9816a98e923689
rafalklepacz <rafal.klepacz@hotmail.com>
2022-12-02 20:46:38 +0100

    Added solutions for day 1

---
+++ 2022/DAY_01_1/Program.cs
@@ -0,0 +1,10 @@
+﻿string inputFile = "input.txt";
+string newLine = "\r\n";
+
+var max = File.ReadAllText(inputFile)
+              .Split($"{newLine}{newLine}")
+              .Where(i => !string.IsNullOrEmpty(i))
+              .Max(i => i.Split(newLine)
+                         .Select(j => int.Parse(j))
+                         .Sum());
+Console.WriteLine(max);
---
+++ 2022/DAY_01_2/Program.cs
@@ -0,0 +1,14 @@
+﻿string inputFile = "input.txt";
+string newLine = "\r\n";
+
+var sum = File.ReadAllText(inputFile)
+              .Split($"{newLine}{newLine}")
+              .Where(i => !string.IsNullOrEmpty(i))
+              .Select(i => i.Split(newLine)
+                            .Select(j => int.Parse(j))
+                            .Sum())
+              .OrderByDescending(_ => _)
+              .Take(3)
+              .Sum();
+
+Console.WriteLine(sum);
