276780d9abba52aaa6d35a549208740f3fe0a558
rafalklepacz <rafal.klepacz@hotmail.com>
2022-12-03 01:17:26 +0100

    Added solutions for day 2

---
+++ 2022/DAY_02_1/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿var inputFile = "input.txt";
+var drawLines = new[] {"A X", "B Y", "C Z"};
+var winLines = new[] {"A Y", "B Z", "C X"};
+
+var sum = File.ReadAllLines(inputFile).AsEnumerable().Sum(line=>
+    line switch
+    {
+        _ when winLines.Contains(line) => 6,
+        _ when drawLines.Contains(line) => 3,
+        _ => 0
+    }
+    + line.Split(' ')[1] switch
+    {
+        "X" => 1,
+        "Y" => 2,
+        "Z" => 3,
+        _ => 0
+    });
+
+Console.WriteLine(sum);
