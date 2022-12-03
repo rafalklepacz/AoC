91a0401fbec7ec4e4ccaff44891ba749c6901c2c
rafalklepacz <rafal.klepacz@hotmail.com>
2022-12-03 11:59:34 +0100

    Added solutions for day 3

---
+++ 2022/DAY_03_1/Program.cs
@@ -0,0 +1,28 @@
+﻿var inputFile = "input.txt";
+
+var result = File.ReadAllLines(inputFile)
+                    .AsEnumerable()
+                    .Select(GetCompartments)
+                    .Select(GetDuplicates)
+                    .Select(d => d.Select(GetValue).Sum())
+                    .Sum();
+
+Console.WriteLine(result);
+
+(string First, string Second) GetCompartments(string rucksack)
+{
+    var half = rucksack.Length / 2;
+    var first = rucksack.Substring(0, half);
+    var second = rucksack.Substring(half, half);
+    return (first, second);
+}
+
+string GetDuplicates((string First, string Second) compartments)
+    =>  new (compartments.First.Intersect(compartments.Second).ToArray());
+
+int GetValue(char c)
+    => c switch
+    {
+        _ when Char.IsLower(c) => c - 96,
+        _ => c - 38,
+    };
---
+++ 2022/DAY_03_2/Program.cs
@@ -0,0 +1,20 @@
+﻿var inputFile = "input.txt";
+
+var result = File.ReadAllLines(inputFile)
+                    .AsEnumerable()
+                    .Chunk(3)
+                    .Select(GetDuplicates)
+                    .Select(d => d.Select(GetValue).Sum())
+                    .Sum();
+
+Console.WriteLine(result);
+
+string GetDuplicates(string[] group)
+    => new(group[0].Intersect(group[1].Intersect(group[2]).Distinct()).ToArray());
+
+int GetValue(char c)
+    => c switch
+    {
+        _ when Char.IsLower(c) => c - 96,
+        _ => c - 38,
+    };
